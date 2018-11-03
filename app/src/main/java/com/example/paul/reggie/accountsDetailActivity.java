94f19ee04d95bbe679d6e960f67c130d83e7b073
ongtm <teeamong@gmai.com>
2018-11-03 11:13:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/paul/reggie/accountsDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.paul.reggie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class accountsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accounts_detail);
+    }
+}
