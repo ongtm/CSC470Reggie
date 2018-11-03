31259fc4ed71894ca46ee78dd52459a363bc6620
ongtm <teeamong@gmai.com>
2018-11-03 12:11:38 -0400

    Update app/src/main/java/com/example/paul/reggie/MainActivity.java
    Testing sync between Reggie 2 android project and CSC470Reggie github repository

--- app/src/main/java/com/example/paul/reggie/MainActivity.java
+++ app/src/main/java/com/example/paul/reggie/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//Adding a change for git test
+//Adding a change for git test again
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
