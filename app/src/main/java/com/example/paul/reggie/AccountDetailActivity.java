6cab7de88fe0b3e3cfc0f84f4a43d7e2432c0bed
ongtm <teeamong@gmai.com>
2018-12-03 23:43:05 -0500

    12032018 9:30PM
    Add preliminary menu bar

--- app/src/main/java/com/example/paul/reggie/AccountDetailActivity.java
+++ app/src/main/java/com/example/paul/reggie/AccountDetailActivity.java
@@ -1,7 +1,12 @@
 package com.example.paul.reggie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AccountDetailActivity extends AppCompatActivity {
 
@@ -10,4 +15,37 @@ public class AccountDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts_detail);
     }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater mInflator = getMenuInflater();
+        mInflator.inflate(R.menu.menu_toolbar,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemID = item.getItemId();
+
+        if(itemID == R.id.menu_viewBudgets){
+            //navigate to score summary
+            startActivity(new Intent(AccountDetailActivity.this,BudgetSummaryActivity.class));
+        }
+        else if (itemID == R.id.menu_howTo){
+            //navigate to about
+            Toast.makeText(this, "This menu item is not operational at this time", Toast.LENGTH_LONG).show();
+        }
+        else{
+            //No action
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
 }
